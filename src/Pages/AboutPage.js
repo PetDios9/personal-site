445dc6f395b3c42fd1bde26a54715d9805b0fcea
Peter Diosdado <diosdadopeter@gmail.com>
2021-07-04 14:31:08 -0700

    fixed vertical scroll, chrome dev tool inspect responsive sizing does not reflect what appears once site is pushed. mini pushes like this are to test unfortunately

--- src/Pages/AboutPage.js
+++ src/Pages/AboutPage.js
@@ -9,7 +9,7 @@ const useStyles = makeStyles(theme=> ({
         width: '100%',
         height: '100%',
         color: 'white ',
-        overflowY: 'hidden'
+        
     },
     container: {
         height: '100vh',
